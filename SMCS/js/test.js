1d6bd2f9b6b0a93e3ff015569898a27aafd8f99b
gokuma <go@kumagai-no-MacBook-Pro.local>
2016-11-08 18:05:18 +0900

    file保存

--- SMCS/js/test.js
+++ SMCS/js/test.js
@@ -28,11 +28,7 @@ recognition.onresult = function(event,$scope) {
 
    		texts.splice(0,0,object);
     	console.log(texts);
-	
-	//ローカルストレージに保存?
-	window.localStorage.setItem("testFile", texts);
-	var val = window.localStrage.getItem("testFile");
-	document.querySelector("#result").innerHTML = val;
+
 	
     	// $scope.texts = texts;
 		recognition.stop();
@@ -104,11 +100,48 @@ function record()
 function stop()
 {
 	var currentTime = new Date();
+	writeToLocal("hoge.txt", "foo\n");
 }
+
 function showHistory()
 {
 	document.getElementById('history').innerHTML = "";
 	for (var i = 0, len = texts.length; i < len; i++) {
 	document.getElementById('history').innerHTML += "<a href='#' class='list-group-item'><span class='badge'>"+texts[i].time+" seconds"+"</span><i class='fa fa-fw fa-comment'></i>"+texts[i].text+"</a>";
 	}
+}
+
+function writeToLocal(filename, content) {
+    // chrome以外は弾く
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.indexOf('chrome') == -1) {
+        alert("This Page is Google Chrome only!");
+    }
+
+    function errorCallback(e) {
+        alert("Error: " + e.name);
+    }
+
+    function fsCallback(fs) {
+        fs.root.getFile(filename, {create: true}, function(fileEntry) {
+            fileEntry.createWriter(function(fileWriter) {
+
+                fileWriter.onwriteend = function(e) {
+                    alert("Success! : " + fileEntry.fullPath);
+                };
+
+                fileWriter.onerror = function(e) {
+                    alert("Failed: " + e);
+                };
+
+                var output = new Blob([content], {type: "text/plain"});
+                fileWriter.write(output);
+            }, errorCallback);
+        }, errorCallback);
+    }
+    // クオータを要求する。PERSISTENTでなくTEMPORARYの場合は
+    // 直接 webkitRequestFileSystem を呼んでよい
+    webkitStorageInfo.requestQuota(PERSISTENT, 1024,
+        webkitRequestFileSystem(PERSISTENT, 1024, fsCallback, errorCallback),
+    errorCallback);
 }
